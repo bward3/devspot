fbbf50e69a606ba6366dcb2b61b0f758cebf4b55
cdfoye <cdfoye@gmail.com>
2022-06-02 20:09:09 -0700

    added loop to in hompage.handlebars to render users

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,30 +1,30 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.get("/", function(req, res) {
-    res.render("homepage");
-})
+router.get("/", function (req, res) {
+  res.render("homepage");
+});
 
-router.get("/login", function(req, res) {
-    res.render("login");
-})
+router.get("/login", function (req, res) {
+  res.render("login");
+});
 
-router.get("/profile", function(req, res) {
-    res.render("profile", {
-        // Example
-        userName: "Bob Smith",
-        expertise: {
-            html: 1,
-            css: 1,
-            ruby: 3,
-            nodeJs: 5,
-            apple: 1,
-            bash: 5,
-        }
-    });
-})
+router.get("/profile", function (req, res) {
+  res.render("profile", {
+    // Example
+    userName: "Bob Smith",
+    expertise: {
+      html: 1,
+      css: 1,
+      ruby: 3,
+      nodeJs: 5,
+      apple: 1,
+      bash: 5,
+    },
+  });
+});
 
-router.get("/edit", function(req, res) {
-    res.render("edit");
-})
+router.get("/edit", function (req, res) {
+  res.render("edit");
+});
 
 module.exports = router;
