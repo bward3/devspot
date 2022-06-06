a61e0aa1715e4cdc95d21b95bbb356c5f66a106a
Kelly Walsh <kellywalsh827@gmail.com>
2022-06-06 10:27:05 -0600

    got login to work, fixed bcrypt hooks, added to profile.js

--- public/js/edit.js
+++ public/js/edit.js
@@ -3,7 +3,6 @@ document.querySelector("#dropdownBtn").addEventListener("click", function() {
     document.querySelector("#dropdown-menu3").classList.remove("is-hidden")
 });
 
-// 
 document.querySelectorAll(".dropdown-item").forEach( function(item) {
     item.addEventListener("click", function(event) {
         document.querySelector("#dropdownTitle").textContent = event.target.textContent
@@ -13,7 +12,18 @@ document.querySelectorAll(".dropdown-item").forEach( function(item) {
 });
 
 // Slider Extension
-bulmaSlider.attach();
+// bulmaSlider.attach();
 
 // document.querySelector('#upload-btn').addEventListener('click', () => {
 // });
+
+
+
+document.querySelector("#editBtn").addEventListener("click", function() {
+    var editProfile = document.querySelector("#editName").value
+    var editLocation = document.querySelector("#editLocation").value
+    var editBio = document.querySelector("#editBio").value
+    var editLinkedIn = document.querySelector("#editLinkedIn").value
+    var editGitHub = document.querySelector("#editGitHub").value
+    // add fethch /api/profile
+});
--- seeds/seed.js
+++ seeds/seed.js
@@ -2,11 +2,20 @@ const sequelize = require('../config/connection');
 const { Profile, User, Tech } = require('../models');
 
 const techSeedData = require('./techSeedData.json');
+const userSeedData = require('./userData.json');
+const profileSeedData = require('./profileData.json');
+
 
 const seedDatabase = async () => {
     await sequelize.sync({ force: true});
 
     const technologies = await Tech.bulkCreate(techSeedData);
+    const user = await User.bulkCreate(userSeedData, {
+        individualHooks: true,
+    });
+
+    const profile = await Profile.bulkCreate(profileSeedData);
+
 }
 
 seedDatabase();
